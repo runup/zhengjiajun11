c762c5375f10b1f2757c13ae0e5e65b801eb4939
runup <645112281@qq.com>
2013-07-17 19:17:51 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhengjiajun::Application.config.secret_token = '5efc840eb2ee3cfa2a146d5c13597c5724878ff36f89da0bce60cc3d726eb5698fe40fbb3b4bf40d038b2dc8bfc8d59092a1116aaff79dd1621ccd8bc353a42d'
